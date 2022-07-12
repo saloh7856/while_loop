b76ec2854e0efd6ec72703ffc4b6de9d8421213d
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-12 06:13:55 +0500

    n sonigacha bulgan juft sonlarni sonini aniqlash

--- bin/while_loop02.dart
+++ bin/while_loop02.dart
@@ -2,4 +2,16 @@
   Create function func with "a " argument
   Return the number of even numbers up to "a".
 */
-void main() {}
+int func(int a){
+  int s=0,k=0,n=2;
+  while(s<a){
+    s=s+n;
+    if(s>0){
+      k+=1;
+    }
+  }
+  return k;
+}
+void main(){
+  print(func(15));
+}
