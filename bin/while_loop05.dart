6c022f5c8e661c61a0e934155849941771de2a93
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-12 06:43:16 +0500

    n sonigacha bulgan toq  sonlar sum dan juft sonlar sum orasidagi farqni aniqlash

--- bin/while_loop05.dart
+++ bin/while_loop05.dart
@@ -2,4 +2,19 @@
   Create function func with "a " argument
   Find the difference between the sum of odd numbers and the sum of even numbers up to a.
 */
-void main() {}
+int func(int a){
+  int s=0,k=0,n=1,m=0;
+  while(s<a){
+    s+=n;
+    if(s%2==1){
+      k+=s;
+    } else 
+    if(s%2==0){
+      m+=s;
+    } 
+  }
+  return k-m;
+}
+void main(){
+  print(func(50));
+}
