06f35a4ae83ad7f4f8d67bdf17d71886ea98362e
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-12 06:20:49 +0500

    n sonigacha bulgan toq sonlar sum  aniqlash

--- bin/while_loop03.dart
+++ bin/while_loop03.dart
@@ -2,4 +2,17 @@
   Create function func with "a " argument
   Return the sum of odd numbers up to "a".
 */
-void main() {}
+int func(int a){
+  int s=0,k=0,n=1;
+  while(s<a){
+    s+=n;
+    if(s%2==1){
+      k+=s;
+    }
+  }
+  return k;
+}
+void main(){
+  print(func(15));
+}
+
