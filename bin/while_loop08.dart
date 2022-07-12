4ab2b08781ba919bf7482e24f6e01fe1bfcfde8c
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-12 10:46:28 +0500

    n soni berilgan u sonni raqamlari  toqlarini topib string kurinishida chop etig

--- bin/while_loop08.dart
+++ bin/while_loop08.dart
@@ -4,4 +4,19 @@
   
   
 */
-void main() {}
+String func(int a){
+  int s=0,n=1;
+  String k,f=' ';
+  while(0<a){
+    s+=n;
+    if(s%2==1){
+      k=s.toString();
+      f+=k;
+    }
+  }
+  return f;
+}
+void main(){
+  print(func(14585546));
+}
+
